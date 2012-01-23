432f7e3eab4d47d9de3a8b56463baa704032338a
Islam Mušić <islam@Islams-MacBook-Pro.local>
2012-01-23 11:41:44 +0100

    Začetni upload2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digg::Application.config.secret_token = 'cd6cc1224c19873f21642218b3c738e81662eff3800663f9ecf80f1c9533fb792c8405639662c2e17d3fb31ef50509206cc030d9751ee610991206a244d2e2af'
